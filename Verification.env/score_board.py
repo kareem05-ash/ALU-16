b439df03ab672e56da0d5d716b3ec93e276ae828
Kareem Ashraf <ashrafkareem825@gmail.com>
2025-11-27 15:46:46 +0200

    edit score_board.py: add cleaning logs script

--- Verification.env/score_board.py
+++ Verification.env/score_board.py
@@ -2,6 +2,20 @@
 
 from golden_model import alu_golden_model as gm
 
+# clean logs
+ls = []
+with open('.\\logs.log', 'r+') as f:
+  lines = f.readlines()
+  # Uncomment all logs
+  for line in lines:
+    line = line[2:] if line.startswith('# ') else line
+    if '|' in line:
+      if len(line.split('|')) == 6:
+        ls.append(line)
+        
+with open('.\\logs.log', 'w') as f:
+  f.writelines(ls)
+
 # read logs
 with open(".\\logs.log", 'r') as f:
   lines = f.readlines()
@@ -24,16 +38,16 @@ All, PASSed = 0, 0
 def comp(A, B, Cin, F, Status, exp_Status, Result, exp_Result):
   global All, PASSed
   All += 1
-  if (Status == exp_Status) and (Result == exp_Result):
+  if Status == exp_Status and Result == exp_Result:
     PASSed += 1
     print(f"[PASS] | A = {'0x' + str(hex(A))[2:].zfill(4).upper()}, B = {'0x' + str(hex(B))[2:].zfill(4).upper()}", end=', ')
     print(f"Cin = {Cin}, OpCode = {'0b\'' + str(bin(F))[2:].zfill(5)}", end=', ')
-    print(f"Status = {'0b\'' + str(bin(Status))[2:].zfill(6)}, Expected = {'0b\'' + str(bin(exp_Status))[2:].zfill(6)}", end=' | ')
+    print(f"Status = {'0b\'' + str(bin(status))[2:].zfill(6)}, Expected = {'0b\'' + str(bin(exp_Status))[2:].zfill(6)}", end=' | ')
     print(f"Result = {'0x' + str(hex(Result))[2:].zfill(4).upper()}, Expected = {'0x' + str(hex(exp_Result))[2:].zfill(4).upper()}")
   else:
     print(f"[FAIL] | A = {'0x' + str(hex(A))[2:].zfill(4).upper()}, B = {'0x' + str(hex(B))[2:].zfill(4).upper()}", end=', ')
     print(f"Cin = {Cin}, OpCode = {'0b\'' + str(bin(F))[2:].zfill(5)}", end=', ')
-    print(f"Status = {'0b\'' + str(bin(Status))[2:].zfill(6)}, Expected = {'0b\'' + str(bin(exp_Status))[2:].zfill(6)}", end=' | ')
+    print(f"Status = {'0b\'' + str(bin(status))[2:].zfill(6)}, Expected = {'0b\'' + str(bin(exp_Status))[2:].zfill(6)}", end=' | ')
     print(f"Result = {'0x' + str(hex(Result))[2:].zfill(4).upper()}, Expected = {'0x' + str(hex(exp_Result))[2:].zfill(4).upper()}")
     
 # Calculate
